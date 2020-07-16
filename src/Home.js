7a1b00f542656857c52fa0a949e59f8e1e16deb6
abhay.baiju <abhay0198@gmail.com>
2020-07-16 00:51:14 -0700

    Creates routes, navbar and banner (#2)

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Home.css';
+function Home() {
+    return (
+        <div className="home">
+            <img
+            className="home_image"
+            src = "https://images-na.ssl-images-amazon.com/images/G/01/AmazonExports/Fuji/2020/March/Fuji_TallHero_ShipATW_Modified_en_US_1x._CB419795291_.jpg"
+            alt="Ship Global"></img>
+        </div>
+    )
+}
+
+export default Home;
